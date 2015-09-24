40404619f66f92bae654c881f06b0547f877725d
r00ky <r00kium@googlemail.com>
2015-09-24 23:20:29 +0200

    updated read to buffer with byte count consideration

--- unix.go
+++ unix.go
@@ -224,13 +224,12 @@ func (connection *Connection) Read(delimiter byte) ([]byte, error) {
 // ReadToBuffer reads from an open connection into a []byte buffer with the given size.
 func (connection *Connection) ReadToBuffer(size int) ([]byte, error) {
 	buffer := make([]byte, size)
-	//TODO do something with bytes read
-	_, err := connection.f.Read(buffer)
+	n, err := connection.f.Read(buffer)
 	if err != nil {
 		return nil, err
 	}
 
-	return buffer, nil
+	return buffer[:n], nil
 }
 
 // Flush the connection, which causes untransmitted or not read data to be discarded.
